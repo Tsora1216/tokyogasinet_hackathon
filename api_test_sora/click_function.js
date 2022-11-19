976d21da5bb238a5ccfe2ed9405c8a83a7b0965f
Tsora1216 <so12ra16@icloud.com>
2022-11-19 16:42:32 +0900

    main

---
+++ api_test_sora/click_function.js
@@ -0,0 +1,19 @@
+
+let init_amount = 500;
+
+function increase_one(){
+    init_amount += 1;
+    document.getElementById('amount').innerHTML = init_amount;
+}
+function increase_ten(){
+    init_amount += 10;
+    document.getElementById('amount').innerHTML = init_amount;
+}
+function increase_hundred(){
+    init_amount += 100
+    document.getElementById('amount').innerHTML = init_amount;
+}
+function increase_fivehundred(){
+    init_amount += 500;
+    document.getElementById('amount').innerHTML = init_amount;
+}
