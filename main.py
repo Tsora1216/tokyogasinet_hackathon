d2d8c6b9648a580d4fe030b105b5b61ad4ced0a1
shake150 <t120073@ed.sus.ac.jp>
2022-11-19 18:36:45 +0900

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template ,request
+import requests
 
 #Flaskのコンストラクタ
 app = Flask(__name__,static_folder="static")
@@ -13,21 +14,24 @@ def top():
         title="サンプル",
         message = "あいうえお"
     )
-#プログラム起動
-app.run(host="localhost",port=5000,debug=True)
 
 
-@app.route("/sample")
-def top2():
-    
-    num = 1+1;
+@app.route("/change")
+def change():
+    money = 50
+    url = 'https://toukyogasinethackathon.azurewebsites.net/api/HttpTrigger2?code=7KI8PBRSyakmetbKcuBjAeZLmU2BArfCBJ3SEQBfMPJNAzFu6MOBJQ==&name='+str(money)
+    response = requests.get(url)
+    print(response.text)
+    num= response.text
+    print(num)
+
 
     return render_template( 
         "index.html",
         title="サンプル",
         message = f"{num}"
-
     )
+
 #プログラム起動
 app.run(host="localhost",port=5000,debug=True)
 
